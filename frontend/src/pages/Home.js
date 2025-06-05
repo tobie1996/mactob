87f3876cc8a42a33b6c5b822c8073c6b95b50d2b
ekass1996 <tobiemba@gmail.com>
2025-06-05 11:28:34 +0100

    commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import HorizontalCardProduct from '../components/HorizontalCardProduct';
 import VerticalCardProduct from '../components/VerticalCardProduct';
 import Benefits from '../components/Benefits';
@@ -28,59 +28,18 @@ const Home = () => {
     { category: "ACCESSOIRES", heading: "Accessoires indispensables pour vos appareils" }
   ];
 
-  const itemsPerPage = 5;
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = Math.ceil(products.length / itemsPerPage);
-  
-  const currentProducts = products.slice(
-    (currentPage - 1) * itemsPerPage,
-    currentPage * itemsPerPage
-  );
-
-  const goToPage = (pageNumber) => setCurrentPage(pageNumber);
-
   return (
     <div>
-       <Banner />
+      <Banner />
       <Title />
       
-      {currentProducts.length > 0 ? (
-        currentProducts.map((product, index) => (
+      {products.length > 0 ? (
+        products.map((product, index) => (
           <VerticalCardProduct key={index} category={product.category} heading={product.heading} />
         ))
       ) : (
         <div className="text-center text-gray-500 mt-8 font-semibold">
           Aucun produit disponible pour le moment.
-        </div>
-      )}
-
-      {products.length > 0 && (
-        <div className="flex justify-center mt-8 font-semibold">
-          <button
-            className={`px-4 py-2 border rounded-lg ${currentPage === 1 ? 'bg-gray-300 cursor-not-allowed' : 'bg-orange-500 text-white'}`}
-            onClick={() => goToPage(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            Retour
-          </button>
-
-          {[...Array(totalPages)].map((_, i) => (
-            <button
-              key={i + 1}
-              className={`px-4 py-2 border rounded-lg ${currentPage === i + 1 ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'}`}
-              onClick={() => goToPage(i + 1)}
-            >
-              {i + 1}
-            </button>
-          ))}
-
-          <button
-            className={`px-4 py-2 border rounded-lg ${currentPage === totalPages ? 'bg-gray-300 cursor-not-allowed' : 'bg-orange-500 text-white'}`}
-            onClick={() => goToPage(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            Suivant
-          </button>
         </div>
       )}
     </div>
